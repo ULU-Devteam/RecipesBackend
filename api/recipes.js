f482204ebd60743ccae0475ffeed306d5afa3bf4
Mika Krooswijk <mikakrooswijk@live.nl>
2017-11-23 18:13:03 +0100

    add recipe works

--- api/recipes.js
+++ api/recipes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const routes = express.Router();
-const Recipe = require('../model/recipe.model')
+const Recipe = require('../model/recipe.model');
 const mongodb = require('../config/mongo.db');
-const Ingriedient = require('../model/ingredient.model')
+const Ingriedient = require('../model/ingredient.model');
 
 routes.get('/recipes', function(req, res) {
     res.status(200).json('die shit werkt')
@@ -11,31 +11,29 @@ routes.get('/recipes', function(req, res) {
 routes.post('/recipes', (req, res) =>{
     reqRecipe = req.body;
     let ingredientIds = [];
+    promiseArray = [];
 
     for(let i of reqRecipe.ingredients){
-        let ingredient = new Ingriedient(i)
-        ingredient.save()
-            .then((ing) => {
-                ingredientIds.push(ing._id);
-
-            })
+        let ingredient = new Ingriedient(i);
+        promiseArray.push(ingredient.save());
     }
 
-    reqRecipe.ingredients = ingredientIds
-    let recipe = new Recipe(reqRecipe);
+    Promise.all(promiseArray)
+        .then((result) => {
+            for(let r of result){
+                ingredientIds.push(r._id);
+            }
+            reqRecipe.ingredients = ingredientIds;
+            let recipe = new Recipe(reqRecipe);
 
-    recipe.save()
-        .then((recipe) => {
-            res.status(200).json(recipe);
-        })
-        .catch((error) =>{
-            res.status(400).json(error);
-        })
+            recipe.save()
+                .then((recipe) => {
+                    res.status(200).json(recipe);
+                })
+                .catch((error) =>{
+                    res.status(400).json(error);
+                });
+        });
+});
 
-
-
-
-
-})
-
-module.exports  = routes
+module.exports  = routes;
