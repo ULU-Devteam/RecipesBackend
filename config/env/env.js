332f3f88de56bb53f09630d0d0db61a545dccec2
Mark <mamjong@avans.nl>
2017-11-23 17:19:38 +0100

    Testing database setup in progress...
    shopping-list collection renamed to shoppingList, added test global variable.

--- config/env/env.js
+++ config/env/env.js
@@ -9,7 +9,8 @@ let env = {
 
 let dburl = process.env.NODE_ENV === 'production' ?
     'mongodb://' + env.dbUser + ':' + env.dbPassword + '@' + env.dbHost + ':' + env.dbPort + '/' + env.dbDatabase :
-    'mongodb://localhost/' + env.dbDatabase
+    process.env.NODE_ENV === 'test' ? 'mongodb://localhost/recipemongodb_test' :
+    'mongodb://localhost/' + env.dbDatabase;
 
 module.exports = {
     env: env,
