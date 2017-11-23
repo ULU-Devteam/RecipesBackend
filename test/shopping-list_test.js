682cae6539dc1490a83665d0731af71a702e4f36
Mark <mamjong@avans.nl>
2017-11-23 21:27:21 +0100

    Added shopping-list post test.
    Mocha test for posting an existing ingredient to the shopping-list.

--- test/shopping-list_test.js
+++ test/shopping-list_test.js
@@ -15,17 +15,39 @@ describe('Querying the shopping-list in the database successfully', () => {
 	});
 
 	it('returns a shopping-list with ingredients', (done) => {
-		ShoppingList.findOne({})
+		ShoppingList.findOne()
 			.populate('ingredients')
 			.then((shoppingList) => {
-			console.log(shoppingList);
 			assert(shoppingList.name === 'testlist');
 			assert(shoppingList.ingredients[0].name === 'testingredient');
 			done();
 			});
 	});
 
-	it('posts a ingredient to the shopping-list', (done) => {
+
+	it('posts an ingredient to the shopping-list', (done) => {
+		const postIngredient = new Ingredient({ name: 'postedIngredient', amount: 6 });
+
+		postIngredient.save().then((result) => {
+			Promise.all([
+				ShoppingList.findOne().populate('ingredients'),
+				Ingredient.findById(result._id)
+			]).then((values) => {
+				const list = values[0];
+				const ingredient = values[1];
+
+				list.ingredients.push(ingredient);
+				list.save()
+					.then((savedList) => {
+						assert(savedList.ingredients.length === 2);
+						assert(savedList.ingredients[1].name === 'postedIngredient');
+						done();
+				});
+			});
+		});
+	});
+
+	it('removes an ingredient to the shopping-list', (done) => {
 		done();
 	});
 });
