e0849f384d8e733efe526949f0268f822eaaa059
Mika Krooswijk <mikakrooswijk@live.nl>
2017-11-23 18:03:12 +0100

    ingredient model

--- api/recipes.js
+++ api/recipes.js
@@ -2,13 +2,28 @@ const express = require('express');
 const routes = express.Router();
 const Recipe = require('../model/recipe.model')
 const mongodb = require('../config/mongo.db');
+const Ingriedient = require('../model/ingredient.model')
 
 routes.get('/recipes', function(req, res) {
     res.status(200).json('die shit werkt')
 });
 
 routes.post('/recipes', (req, res) =>{
-    let recipe = new Recipe(req.body);
+    reqRecipe = req.body;
+    let ingredientIds = [];
+
+    for(let i of reqRecipe.ingredients){
+        let ingredient = new Ingriedient(i)
+        ingredient.save()
+            .then((ing) => {
+                ingredientIds.push(ing._id);
+
+            })
+    }
+
+    reqRecipe.ingredients = ingredientIds
+    let recipe = new Recipe(reqRecipe);
+
     recipe.save()
         .then((recipe) => {
             res.status(200).json(recipe);
@@ -16,6 +31,11 @@ routes.post('/recipes', (req, res) =>{
         .catch((error) =>{
             res.status(400).json(error);
         })
+
+
+
+
+
 })
 
 module.exports  = routes
---
+++ model/ingredient.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const IngredientSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    }
+});
+
+const Ingredient = mongoose.model('ingredient', IngredientSchema);
+
+module.exports = Ingredient;
