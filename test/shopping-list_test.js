1866795051a5939e91db893e2ce6b20e7891f3ad
Mark <mamjong@avans.nl>
2017-11-24 13:26:49 +0100

    Added mocha tests for remaining shopping-list endpoints.
    Updated POST to work with new subdocument. Added PUT test and two REMOVE tests for each endpoint.

--- test/shopping-list_test.js
+++ test/shopping-list_test.js
@@ -6,7 +6,7 @@ describe('Querying the shopping-list in the database successfully', () => {
 	let list;
 
 	beforeEach((done) => {
-		list = new ShoppingList({ name: 'testlist', ingredients: [{ name: 'testingredient', amount: 3}] });
+		list = new ShoppingList({name: 'testlist', ingredients: [{name: 'testingredient', amount: 3}]});
 
 		list.save(() => done())
 	});
@@ -14,32 +14,74 @@ describe('Querying the shopping-list in the database successfully', () => {
 	it('returns a shopping-list with ingredients', (done) => {
 		ShoppingList.findOne()
 			.then((shoppingList) => {
-			assert(shoppingList.name === 'testlist');
-			assert(shoppingList.ingredients[0].name === 'testingredient');
-			done();
+				assert(shoppingList.name === 'testlist');
+				assert(shoppingList.ingredients[0].name === 'testingredient');
+				done();
 			});
 	});
 
 
 	it('posts an ingredient to the shopping-list', (done) => {
-		const postBody = [{ name: 'postTest1', amount: 2 }, { name: 'postTest2', amount: 3 }]
+		const postBody = [{name: 'postTest1', amount: 2}, {name: 'postTest2', amount: 3}];
 
 		ShoppingList.findOne()
 			.then((shoppingList) => {
-			for (let item of postBody) {
-				shoppingList.ingredients.push(item)
-			}
+				for (let item of postBody) {
+					shoppingList.ingredients.push(item)
+				}
 
-			shoppingList.save()
-				.then((savedList) => {
-				assert(savedList.ingredients.length === 3);
-				assert(savedList.ingredients[1].name === 'postTest1');
-				done();
-				});
+				shoppingList.save()
+					.then((savedList) => {
+						assert(savedList.ingredients.length === 3);
+						assert(savedList.ingredients[1].name === 'postTest1');
+						done();
+					});
 			});
 	});
 
-	it('removes an ingredient to the shopping-list', (done) => {
-		done();
+	it('updates an ingredient in the shopping-list', (done) => {
+		const updateIngredient = {name: 'updatedIngredient', amount: 12};
+
+		ShoppingList.findOne()
+			.then((shoppingList) => {
+				shoppingList.ingredients.id(shoppingList.ingredients[0]._id).set(updateIngredient);
+				shoppingList.save()
+					.then((savedList) => {
+						assert(savedList.ingredients[0].name === 'updatedIngredient');
+						assert(savedList.ingredients[0].amount === 12);
+						done();
+					});
+			});
+	});
+
+	it('removes an ingredient from the shopping-list', (done) => {
+
+		ShoppingList.findOne()
+			.then((shoppingList) => {
+				shoppingList.ingredients.id(shoppingList.ingredients[0]._id).remove()
+					.then((result) => {
+						assert(result === undefined);
+					});
+				return shoppingList;
+			})
+			.then((shoppingList) => {
+				shoppingList.save()
+					.then((result) => {
+						assert(result.ingredients.length === 0);
+						done();
+					});
+			});
+	});
+
+	it('empties the entire ingredients array', (done) => {
+		ShoppingList.findOne()
+			.then((shoppingList) => {
+			shoppingList.ingredients = [];
+			shoppingList.save()
+				.then((savedList) => {
+				assert(savedList.ingredients.length === 0);
+				done();
+				});
+			});
 	});
 });
